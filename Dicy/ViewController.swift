c468fda5f7cc1a889762e846be4744fd6ee7d095
Jörg <bytepunch@gmail.com>
2019-08-05 15:45:32 +0200

    Using an array instead of switch

--- Dicy/ViewController.swift
+++ Dicy/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    let dices = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,43 +35,8 @@ class ViewController: UIViewController {
             let dice1 = Int.random(in: 0...5)
             let dice2 = Int.random(in: 0...5)
             
-            switch dice1 {
-            case 0:
-                diceImageView1.image = UIImage(named: "dice1")
-            case 1:
-                diceImageView1.image = UIImage(named: "dice2")
-            case 2:
-                diceImageView1.image = UIImage(named: "dice3")
-            case 3:
-                diceImageView1.image = UIImage(named: "dice4")
-            case 4:
-                diceImageView1.image = UIImage(named: "dice5")
-            case 5:
-                diceImageView1.image = UIImage(named: "dice6")
-            default:
-                break
-            }
-            
-            switch dice2 {
-            case 0:
-                diceImageView2.image = UIImage(named: "dice1")
-            case 1:
-                diceImageView2.image = UIImage(named: "dice2")
-            case 2:
-                diceImageView2.image = UIImage(named: "dice3")
-            case 3:
-                diceImageView2.image = UIImage(named: "dice4")
-            case 4:
-                diceImageView2.image = UIImage(named: "dice5")
-            case 5:
-                diceImageView2.image = UIImage(named: "dice6")
-            default:
-                break
-            }
-            
-            
-            //print("Shaked!")
-            
+            diceImageView1.image = UIImage(named: dices[dice1])
+            diceImageView2.image = UIImage(named: dices[dice2])
             
         }
     }
